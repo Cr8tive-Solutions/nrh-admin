0c951ffa7f123a538e338cabba059ceca4831029
anwarcr8tive <70792793+anwarcr8tive@users.noreply.github.com>
2026-09-04 10:05:05 +0800

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,8 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,8 +14,15 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin.auth' => \App\Http\Middleware\AdminAuth::class,
             'admin.can'  => \App\Http\Middleware\AdminCan::class,
         ]);
-
         $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                Request::HEADER_X_FORWARDED_HOST |
+                Request::HEADER_X_FORWARDED_PORT |
+                Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
